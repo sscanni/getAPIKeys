d2c296c88195509d379830d4ba75620231678507
sscanni <steven.scanniello@snhu.edu>
2018-10-26 11:10:08 -0400

    Changed database structure for APIKeys

--- readkey.py
+++ readkey.py
@@ -1,12 +1,13 @@
 from getAPIKeys import getAPIKey
 
-apptab = getAPIKey("Google", "GeocodeLocation")
-print (apptab.key1)
-print (apptab.value1)
-print (apptab.key2)
-print (apptab.value2)
-apptab = getAPIKey("Foursquare", "Foursquare")
-print (apptab.key1)
-print (apptab.value1)
-print (apptab.key2)
-print (apptab.value2)
+print ("------Test getAPIKeys Function------")
+print
+google_api_key = getAPIKey("Google", "GeocodeLocation", "google_api_key")
+print ("google_api_key=%s" % (google_api_key))
+print
+foursquare_client_id = getAPIKey("Foursquare", "Foursquare", "client_id")
+print ("foursquare_client_id=%s" % (foursquare_client_id))
+print
+foursquare_client_secret = getAPIKey("Foursquare", "Foursquare", "client_secret")
+print ("foursquare_client_secret=%s" % (foursquare_client_secret))
+print
